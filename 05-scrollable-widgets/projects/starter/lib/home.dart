bd7f55413a0dd4d27a0477290bb1dc6ce2e63abb
Tin Dao <tin.daonguyen@gmail.com>
2021-12-27 16:40:30 +0700

    Finish scrollable-widgets project

--- 05-scrollable-widgets/projects/starter/lib/home.dart
+++ 05-scrollable-widgets/projects/starter/lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'screens/explore_screen.dart';
+import 'screens/recipes_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,10 +13,8 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   static List<Widget> pages = <Widget>[
-    // TODO: Replace with ExploreScreen
-    Container(color: Colors.white),
-    // TODO: Replace with RecipesScreen
-    Container(color: Colors.green),
+    ExploreScreen(),
+    RecipesScreen(),
     Container(color: Colors.blue),
   ];
 
